eb8de0e51ac41eac214e3a06f708f1ae31cba743
Abdul Aziz <abdulaziz758015@gmail.com>
2025-12-03 11:46:58 +0600

    fixed responsiveness

--- src/components/GadgetsList.jsx
+++ src/components/GadgetsList.jsx
@@ -75,7 +75,7 @@ export default function GadgetsList() {
       </div>
 
     
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4'>
         {sortedGadgets.map((gadget) => (
 
           <div 
